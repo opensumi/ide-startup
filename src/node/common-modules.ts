b146b8c1bbc9ce8e4740f117f08eea87f2e588b1
大表哥 <xubing.bxb@alibaba-inc.com>
2022-05-23 18:01:23 +0800

    fix: node module import paths

--- src/node/common-modules.ts
+++ src/node/common-modules.ts
@@ -2,13 +2,13 @@ import { NodeModule, ConstructorOf } from '@opensumi/ide-core-node';
 import { ServerCommonModule } from '@opensumi/ide-core-node';
 import { FileServiceModule } from '@opensumi/ide-file-service/lib/node';
 import { OpenerModule } from '@opensumi/ide-remote-opener/lib/node';
-import { ProcessModule } from '@opensumi/ide-process';
-import { FileSearchModule } from '@opensumi/ide-file-search';
-import { SearchModule } from '@opensumi/ide-search';
+import { ProcessModule } from '@opensumi/ide-process/lib/node';
+import { FileSearchModule } from '@opensumi/ide-file-search/lib/node';
+import { SearchModule } from '@opensumi/ide-search/lib/node';
 import { TerminalNodePtyModule } from '@opensumi/ide-terminal-next/lib/node';
 import { LogServiceModule } from '@opensumi/ide-logs/lib/node';
 import { ExtensionModule } from '@opensumi/ide-extension/lib/node';
-import { OpenVsxExtensionManagerModule } from '@opensumi/ide-extension-manager';
+import { OpenVsxExtensionManagerModule } from '@opensumi/ide-extension-manager/lib/node';
 import { FileSchemeNodeModule } from '@opensumi/ide-file-scheme/lib/node';
 import { AddonsModule } from '@opensumi/ide-addons/lib/node';
 
