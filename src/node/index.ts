63154f16a153b78e3c05b82b84c43c689e8e62f5
大表哥 <xubing.bxb@alibaba-inc.com>
2022-05-23 19:21:55 +0800

    fix: node module import paths

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,5 +1,5 @@
 import { startServer } from './start-server';
-import { ExpressFileServerModule } from '@opensumi/ide-express-file-server';
+import { ExpressFileServerModule } from '@opensumi/ide-express-file-server/lib/node';
 import { CommonNodeModules } from '../../src/node/common-modules';
 
 startServer({
